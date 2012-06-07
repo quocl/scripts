ecfc41605cc8422fe6610dbb5d30530968f37cb7
stryper <user@example.com>
2012-06-07 19:15:41 -0400

    extract

---
+++ bash/extract.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+# script to extract archive files in Linux
+
+if [ -z $1 ]; then
+	echo "No file to extract"
+	echo "Usage: $0 FILE_TO_EXTRACT"
+else
+	case $1 in
+		*.zip)
+			unzip $1;;
+		*.tar.gz)
+			tar -zxvf $1;;
+		*.tgz)
+			tar -zxvf $1;;
+		*.tar.bz2)
+			tar -jxvf $1;;
+		*.tbz2)
+			tar -jxf $1;;
+		*.tar)
+			tar -vxf $1;;
+		*.tar.xz)
+			 tar -vxf $1;;
+		*)
+			echo "Sorry I cannot extract $1. :-("
+	esac		
+fi
